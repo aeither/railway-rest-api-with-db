eff0a864b76e6ff8531a94684aff9a9e67125c01
aeither <36173828+aeither@users.noreply.github.com>
2024-08-16 20:26:01 +0200

    updates

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,10 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:
+    process.env.NODE_ENV === "development"
+      ? process.env.DATABASE_PUBLIC_URL
+      : process.env.DATABASE_URL,
 });
 
 export const db = drizzle(pool);
